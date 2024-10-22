292de81a63b7063e21adf576cbc099a5708f4e17
Kyle Bautista <[kylealexanderbautista09@gmail.com]>
2024-10-21 20:37:15 -0400

    refactored line spin

---
+++ spinner2.js
@@ -0,0 +1,11 @@
+const spinner = ['|', '/', '-', '\\', '|', '/', '-', '\\', '|'];
+let delay = 100;
+for (const spin of spinner) {
+  setTimeout(() => {
+    process.stdout.write(`\r${spin}    `);
+  }, delay);
+  delay += 200;
+}
+setTimeout(() => {
+  process.stdout.write('\n');
+}, delay);
